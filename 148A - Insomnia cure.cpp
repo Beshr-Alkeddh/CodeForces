db06bd6baa7e64eb320a71d6efa8c204b2a6ed1b
Beshr Alkeddh <85810949+Beshr-Alkeddh@users.noreply.github.com>
2021-09-08 00:57:14 +0300

    Update and rename main.cpp to 148A - Insomnia cure.cpp

--- 148A - Insomnia cure.cpp
+++ 148A - Insomnia cure.cpp
@@ -4,17 +4,14 @@ int main()
 {
     int k , l , m , n , d , s = 0 ;
     cin >> k >> l >> m >> n >> d;
+    for (int i = 1 ; i <= d ; i++)
+    {
 
-        for (int i = 1 ; i <= d ; i++)
-        {
-
-           if(i%k != 0 && i%l !=0 && i%m != 0 && i%n != 0)
-                s++;
-        }
-
-     s = d - s;
-
+        if(i%k != 0 && i%l !=0 && i%m != 0 && i%n != 0)
+             s++;
+    }
+    s = d - s;
     cout << endl << s << endl;
-
+    
     return 0;
 }
